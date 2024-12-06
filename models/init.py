e138b937e1d1e6dacca80c0d6849280c4d1dab9e
darkdrayk <blackdrayk@gmail.com>
2024-12-06 11:00:08 +0100

    init.py

---
+++ models/init.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+#from models.sessions import Base as SessionBase
+#from models.users import Base as UserBase
+import os
+
+#variable de entorno:External Database URL
+datadase_url = os.getenv("DATABASE_URL") 
+
+engine = create_engine(datadase_url)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+inspector = inspect(engine)
+
+#creacion tablas basadas en modelos
+def initialize_database():
+    pass
+    #UserBase.metadata.create_all(bind=engine)
+    #SessionBase.metadata.create_all(bind=engine)
