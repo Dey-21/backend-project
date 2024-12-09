2df4423c42f51c7348839afc4cbfe202d939bdde
Dey-21 <anruriv@gmail.com>
2024-12-09 12:22:29 +0100

    Actualización

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://admin21:d210e280y@project-db.cnqowac8sq1o.eu-north-1.rds.amazonaws.com/project-db"
+DATABASE_URL = "postgresql://admin21:d210e280y@proyecto-db.cnqowac8sq1o.eu-north-1.rds.amazonaws.com/proyecto-db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
 from app.database import Base, engine
 from app.models.user import User
 
+# Crear todas las tablas definidas en los modelos
 Base.metadata.create_all(bind=engine)
+
+print("Tablas creadas con éxito.")
