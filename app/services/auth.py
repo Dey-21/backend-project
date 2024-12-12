412ac35e20cda3b4c967861cf78f5e952c983895
darkdrayk <blackdrayk@gmail.com>
2024-12-12 09:19:30 +0100

    agregada función de verificación de usuario

--- app/services/auth.py
+++ app/services/auth.py
@@ -1,8 +1,20 @@
+import jwt
+from datetime import datetime, timedelta
 from sqlalchemy.exc import IntegrityError
 from werkzeug.security import generate_password_hash, check_password_hash
 from fastapi import HTTPException
 from app.models import User
 from app.database import AsyncSession
+
+SECRET_KEY = "secret"
+ALGORITHM = "HS256"
+#ACCESS_TOKEN_EXPIRE = 60   # Duración token en minutos
+
+def create_access_token(data: dict, token_expire: int):    # Genera token y tiempo expiración
+    payload = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=token_expire)
+    payload.update({"exp": expire})
+    return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM) 
 
 
 def get_password_hash(password: str):
@@ -34,3 +46,14 @@ async def add_user(db: AsyncSession, username: str, password: str):
 
 async def get_user_by_name(db: AsyncSession, username: str):
     return await db.query(User).filter(User.username == username).first()
+
+
+async def authenticate_user(db: AsyncSession, username: str, password: str):
+    user = get_user_by_name(db, username)
+    if user is None:
+        return {"message": f"The user {username} is not registered in the system"}
+    
+    if verify_password(user.password, password):
+        return create_access_token({'user_id': user.id}, 60)
+    
+    return {"message": "Invalid user password"}
